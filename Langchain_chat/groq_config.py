c6ed30c9ec1d753b3f844a49e957f5826148518d
prabun756 <prabuy21@gmail.com>
2025-06-14 19:08:30 +0530

    Update groq_config.py

--- Langchain_chat/groq_config.py
+++ Langchain_chat/groq_config.py
@@ -3,6 +3,6 @@ from langchain_groq import ChatGroq
 def get_grqu_llm():
     return ChatGroq(
         model_name = "llama3-8b-8192",
-        groq_api_key = "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",
+        groq_api_key = "XYZ",
         temperature=0.5
     )
